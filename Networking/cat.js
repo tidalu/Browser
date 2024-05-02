dd96bbfbafe4f2417f8040759df90a9752396fef
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-05-02 20:32:12 +0200

    concat-stream

---
+++ Networking/cat.js
@@ -0,0 +1,34 @@
+const concat = require('concat-stream');
+const through = require('through2');
+const http = require('http');
+const qs = require('querystring');
+
+var server = http.createServer(function (req, res) {
+  console.log('request acceped:');
+
+  req.pipe(counter()).pipe(concat({ encoding: 'string' }, onbody));
+
+  function onbody(body) {
+    const params = qs.parse(body);
+    console.log(params);
+    res.end('ok\n');
+  }
+
+  function counter () {
+    let size = 0
+    return through( function (buf, enc, next) {
+        size += buf.length  
+        if(size > 20) {
+            next(null, null)
+        } else {
+            next(null, buf)
+        }
+
+    })
+  }
+  
+});
+
+server.listen(5000, () => {
+  console.log('listening');
+});
